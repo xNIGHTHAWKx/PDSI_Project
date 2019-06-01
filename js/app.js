414af27fb7a41b07a3bf7f2e38b949e9273097a9
209412 <45198261+209412@users.noreply.github.com>
2019-06-01 13:16:48 -0700

    convert to dropzone js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,14 @@
-function btnBrowseFiles(ev) {
-	console.log('File(s) borwsed');
-}
 
-function dropHandler(ev) {
-  console.log('File(s) dropped');
-
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
+Dropzone.options.myAwesomeDropzone = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 20, // MB
+  previewTemplate: document.getElementById('drop-template-preview').innerHTML,
+  previewsContainer: ".dropzone-previews",
   
-  if (ev.dataTransfer.items) {
-    // Use DataTransferItemList interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
-      if (ev.dataTransfer.items[i].kind === 'file') {
-        var file = ev.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
-      }
+  accept: function(file, done) {
+    if (file.name.substr(file.name.lastIndexOf('.') + 1) != "zip") {
+      done("????"); // tekst obok w preview???
     }
-  } else {
-    // Use DataTransfer interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-    }
+    else { done("ok"); }
   }
-}
-
-function dragOverHandler(ev) {
-	
-  console.log('File(s) in drop zone'); 
-
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
-}
+};
