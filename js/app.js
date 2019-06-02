1f6c2c09321034da264c4243f7733797607ebea7
Kamil Jurek <informator017@wp.pl>
2019-06-02 20:26:01 +0200

    Fields validatiopn and information about invalid credentials.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
+window.onload = function () {
+    var usersDataArray = [
+        { index: "teacher1", password: "123456", role: "teacher" },
+        { index: "teacher2", password: "123456", role: "teacher" },
+        { index: "student1", password: "123456", role: "student" },
+        { index: "student2", password: "123456", role: "student" }
+    ];
+
+    var firstNameInput = document.getElementById("first-name-input");
+    var lastNameInput = document.getElementById("last-name-input");
+    var indexInput = document.getElementById("index-input");
+    var passwordInput = document.getElementById("password-input");
+    var loginButton = document.getElementById("login-button");
+
+    loginButton.addEventListener("click", function () {
+        usersDataArray.forEach(function (user) {
+            if (user.role === "student" && indexInput.value === user.index && passwordInput.value === user.password) document.getElementById("student-href").click();
+            if (user.role === "teacher" && indexInput.value === user.index && passwordInput.value === user.password) document.getElementById("teacher-href").click();
+            document.getElementById("invalid-credentials").style.display = "block";
+        });
+    });
+
+    firstNameInput.addEventListener("focus", function () {
+        document.getElementById("invalid-credentials").style.display = "none";
+    });
+
+    lastNameInput.addEventListener("focus", function () {
+        document.getElementById("invalid-credentials").style.display = "none";
+    });
+
+    indexInput.addEventListener("focus", function () {
+        document.getElementById("invalid-credentials").style.display = "none";
+    });
+
+    passwordInput.addEventListener("focus", function () {
+        document.getElementById("invalid-credentials").style.display = "none";
+    });
+
+    function alphaOnly(event) {
+        var key = event.keyCode;
+        return ((key >= 65 && key <= 90) || key == 8);
+    }
+}
 
 Dropzone.options.myAwesomeDropzone = {
   paramName: "file", // The name that will be used to transfer the file
@@ -12,23 +55,3 @@ Dropzone.options.myAwesomeDropzone = {
     else { done("ok"); }
   }
 };
-
-window.onload = function () {
-    var usersDataArray = [
-        { index: "teacher1", password: "123456", role: "teacher" },
-        { index: "teacher2", password: "123456", role: "teacher" },
-        { index: "student1", password: "123456", role: "student" },
-        { index: "student2", password: "123456", role: "student" }
-    ];
-
-    var indexInput = document.getElementById("index-input");
-    var passwordInput = document.getElementById("password-input");
-    var loginButton = document.getElementById("login-button");
-
-    loginButton.addEventListener("click", function () {
-        usersDataArray.forEach(function (user) {
-            if (user.role === "student" && indexInput.value === user.index && passwordInput.value === user.password) document.getElementById("student-href").click();
-            if (user.role === "teacher" && indexInput.value === user.index && passwordInput.value === user.password) document.getElementById("teacher-href").click();
-        });
-    });
-}
