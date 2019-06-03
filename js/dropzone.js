0f94ea1b520c436565ef37c6e9d33096f76b9d41
Kamil Jurek <informator017@wp.pl>
2019-06-03 10:30:53 +0200

    Conflict fix

--- js/dropzone.js
+++ js/dropzone.js
@@ -3528,3 +3528,13 @@ function __guardMethod__(obj, methodName, transform) {
     return undefined;
   }
 }
+
+function confirm_exit() {
+  var answer = confirm("Czy na pewno chcesz zakończyć pracę przed końcem czasu?");
+  if (answer) {
+    location.href = "index.html";
+  }
+  else {
+    // nothing
+  }
+}
